f74a2ce1e5074cde0a02274b415b5a7ee87946ca
thdangtr <dang@MacBook-Pro.local>
2026-04-13 22:43:42 +0700

    add country codes

--- src/app/lpk/consts.py
+++ src/app/lpk/consts.py
@@ -2,10 +2,20 @@ from typing import Final
 
 COUNTRY_CODES: Final[dict[str, str]] = {
     "id": "Indonesia",
+    "sg": "Singapore",
     "my": "Malaysia",
-    "ph": "Philipines",
+    "ph": "Philippines",
     "th": "Thailand",
-    "us": "United States",
     "br": "Brazil",
+    "kh": "Cambodia",
+    "in": "India",
+    "ru": "Russia",
+    "us": "United States",
+    "ca": "Canada",
+    "de": "Germany",
+    "mx": "Mexico",
+    "tr": "Turkey",
+    "gb": "United Kingdom",
     "vn": "Vietnam",
+    "sa": "Saudi Arabia",
 }
